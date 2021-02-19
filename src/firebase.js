a1d63d7fe8f6196e1f3af7f7fd4c9daf6593c1e8
Ezhause Digital <pengusahasukses1999@gmail.com>
2021-02-20 06:11:40 +0700

    add firebase config & add react-router-dom, material-ui and styled-components

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBYRL6o71pUBKdoQP-E-4rDIWm-HoYV0QA",
+  authDomain: "slack-clone-c2812.firebaseapp.com",
+  projectId: "slack-clone-c2812",
+  storageBucket: "slack-clone-c2812.appspot.com",
+  messagingSenderId: "346631407121",
+  appId: "1:346631407121:web:cad3ab35d7be856b6f93b0",
+};
+
+export default firebaseConfig;
